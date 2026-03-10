dbf72ff548c560470cc9dd17c9d455bd59fc2375
keysha-adesi <keyshaputrii590@gmail.com>
2026-03-10 20:49:44 +0700

    first commit

---
+++ prak pemdas 3/file3.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+// Prosedur untuk input data
+void inputData(float &berat, float &tinggi) {
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
+
+// Fungsi menghitung BMI
+float hitungBMI(float berat, float tinggi) {
+    return berat / (tinggi * tinggi);
+}
+
+// Fungsi menentukan status BMI
+string statusBMI(float bmi) {
+    if (bmi < 18.5) {
+        return "Berat Badan Kurang";
+    } 
+    else if (bmi < 25) {
+        return "Berat Badan Normal";
+    } 
+    else if (bmi < 30) {
+        return "Berat Badan Kelebihan";
+    } 
+    else {
+        return "Obesitas";
+    }
+}
+
+int main() {
+    float berat, tinggi, bmi;
+
+    cout << "--- Selamat Datang Mahasiswa Kelas A 2025 ---" << endl;
+
+    inputData(berat, tinggi);
+
+    bmi = hitungBMI(berat, tinggi);
+
+    cout << "\n--- Hasil ---" << endl;
+    cout << "BMI Anda : " << bmi << endl;
+    cout << "Status   : " << statusBMI(bmi) << endl;
+
+    return 0;
+}
